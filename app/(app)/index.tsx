bea61def29026146e486cfd7ea27a10d0fd3eca6
Alberto Zaranza <albertozaranza@outlook.com>
2024-05-30 12:16:18 -0300

    feat: setup auth routes

---
+++ app/(app)/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+
+import { useSession } from "../context/auth";
+
+export default function Index() {
+  const { signOut } = useSession();
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text
+        onPress={() => {
+          signOut();
+        }}
+      >
+        Sign Out
+      </Text>
+    </View>
+  );
+}
